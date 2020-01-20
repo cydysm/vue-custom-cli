732bbe7ccdab4323a0964d41db0109e98bb72e9f
chenxuan <t.ccydlj@gmail.com>
2020-01-20 13:35:20 +0800

    format

--- prompts.js
+++ prompts.js
@@ -26,7 +26,7 @@ module.exports = [
   {
     type: "confirm",
     name: "lint",
-    message: "是否需要lint",
+    message: "是否需要lint(默认 是)",
     default: true
   },
   {
@@ -43,21 +43,4 @@ module.exports = [
       }
     ]
   }
-  // {
-  //   type: "list", // 即类型为 选择项
-  //   name: "module", // 名称，作为下面 generator 函数 options 的键
-  //   message: "请选择你要生成的模块", // 提示语
-  //   choices: [
-  //     { name: "Module1", value: "module1" },
-  //     { name: "Module2", value: "module2" },
-  //     { name: "Module3", value: "module3" }
-  //   ],
-  //   default: "module0"
-  // },
-  // {
-  //   type: "input", // 类型为 输入项
-  //   name: "moduleName",
-  //   message: "请输入模块名称",
-  //   default: "myModule"
-  // }
 ];
